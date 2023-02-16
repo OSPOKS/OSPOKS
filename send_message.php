6820d0b491cbb863c64ce5cb895fb60624baf1ef
Thaju <93075984+Thajudecodes@users.noreply.github.com>
2023-02-16 19:17:59 +0530

    Update send_message.php

--- send_message.php
+++ send_message.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$message = $_POST['message'];
-	$to = 'your-email@example.com';
+	$to = 'thajudecodes@gmail.com';
 	$subject = 'Message from ' . $name;
 	$body = "From: $name\nEmail: $email\nMessage:\n$message";
 
